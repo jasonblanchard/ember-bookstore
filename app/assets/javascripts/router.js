ad994a681238288d0771f203b142c259a097fcc8
Jason Blanchard <jas.blanchard@gmail.com>
2014-02-22 08:52:10 -0500

    First commit

---
+++ app/assets/javascripts/router.js
@@ -0,0 +1,5 @@
+// For more information see: http://emberjs.com/guides/routing/
+
+EmberBookstore.Router.map(function() {
+  // this.resource('posts');
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmberBookstore::Application.config.secret_key_base = 'cb885da397ebd305c6a649c9d2133dc50290e8c2049f2b9dc14bf7f748eb08f3f16b44fcf1c0b20f505a4496baa9976e362ad87c444f82d31331e0d1f9bcbec3'
