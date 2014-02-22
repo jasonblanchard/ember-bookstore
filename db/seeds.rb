9187492a9c992b4755005034a5f15dd01d08e0a8
Jason Blanchard <jas.blanchard@gmail.com>
2014-02-22 18:35:52 -0500

    Added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Book.create(:title => "Green Eggs and Ham", :description => 'A cerebral book about a cat and his disdain for breakfast')
+
+Book.create(:title => "Head First: HTML5 Programming", :description => 'Thriller about the future of the "net"')
+
+Book.create(:title => "The Very Hungry Cateriller", :description => "Tiny creature goes on an unbelieveable rampage", :featured => true)
+
+Book.create(:title => "Bluberries for Sale", :description => "A monster peddles blueberries in the field", :featured => true)
