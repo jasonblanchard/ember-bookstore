b48e3c2a99ad80ecdb0a9942b56f07d0b7f33620
Jason Blanchard <jas.blanchard@gmail.com>
2014-02-26 08:44:42 -0500

    Fixed dates

--- app/assets/javascripts/controllers/reviews_controller.js
+++ app/assets/javascripts/controllers/reviews_controller.js
@@ -1,4 +1,8 @@
 EmberBookstore.ReviewsController = Ember.ArrayController.extend({
+  
+  sortProperties: ['created_at'],
+  sortAscending: false,
+
   reviewCount: function() {
       return this.get('length');
   }.property('length')
--- app/assets/javascripts/models/review.js
+++ app/assets/javascripts/models/review.js
@@ -3,5 +3,6 @@
 EmberBookstore.Review = DS.Model.extend({
   body: DS.attr('string'),
   book: DS.belongsTo('book'),
+  // TODO: Make this camel case
   created_at: DS.attr('date')
 });
