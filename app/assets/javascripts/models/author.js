bb8b7d52f636297b7967db5681c87c356b896f80
Jason Blanchard <jas.blanchard@gmail.com>
2014-03-01 11:39:34 -0500

    Added fullname property

--- app/assets/javascripts/models/author.js
+++ app/assets/javascripts/models/author.js
@@ -1,4 +1,7 @@
 EmberBookstore.Author = DS.Model.extend({
   firstName: DS.attr('string'),
-  lastName: DS.attr('string')
+  lastName: DS.attr('string'),
+  fullName: function() {
+    return this.get('firstName') + " " + this.get('lastName');
+  }.property('firstName', 'lastName')
 });
